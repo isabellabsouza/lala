7bc324519ec894b9ba5a0e8c0e3f72c5c287b428
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2023-05-16 14:54:08 -0300

    Initial commit

---
+++ src/main/java/com/post/teste/lala/model/Status.java
@@ -0,0 +1,8 @@
+package com.post.teste.lala.model;
+
+public enum Status {
+    
+    APROVADO,
+    REPROVADO,
+    EM_ANALISE;
+}
---
+++ src/test/java/com/post/teste/lala/LalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post.teste.lala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
