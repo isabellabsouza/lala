a6c099f29e7d925acb83728c7e35e62d27e71535
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2023-06-10 11:08:03 -0300

    login funciona

--- src/main/java/com/eee/teste/lala/WebSecurityConfig.java
+++ src/main/java/com/eee/teste/lala/WebSecurityConfig.java
@@ -9,22 +9,38 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig{
     
     @Autowired
     private DataSource dataSource;
+
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    //     http
+    //     .authorizeRequests()
+    //         .antMatchers("/home").permitAll()
+    //         .anyRequest().authenticated()
+    //         .and()
+    //     .formLogin()
+    //         .loginPage("/login")
+    //         .defaultSuccessUrl("/pesquisaIngresso", true)
+    //         .permitAll()
+    //         .and()
+    //     .logout()
+    //         .logoutUrl("/logout")
+    //         .logoutSuccessUrl("/home")
+    //         .and()
+    //     .csrf().disable();
+    // }
     
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, UserDetailsService users) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((authz) -> authz
                 .requestMatchers("/home").permitAll()
@@ -36,11 +52,12 @@ public class WebSecurityConfig {
                 .defaultSuccessUrl("/pesquisaIngresso", true)
             )
             .logout(logout -> logout.logoutUrl("/logout"))
-            .rememberMe((rememberMe) -> rememberMe.userDetailsService(users))
             .csrf().disable();
                
         return http.build();
     }
+
+    
 
     
 
@@ -78,20 +95,65 @@ public class WebSecurityConfig {
     //     return new BCryptPasswordEncoder();
     // }
 
-    @Bean
-    UserDetailsManager users(DataSource dataSource) {
-        // PasswordEncoder encoder = new BCryptPasswordEncoder();
+    //INMEMORY USER QUE FUNCIONA
+    // @Bean
+    // UserDetailsManager users(DataSource dataSource) {
+    //     // PasswordEncoder encoder = new BCryptPasswordEncoder();
         
-        UserDetails user = User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("admin")
-            .roles("ADMINISTRADOR")
-            .build();
+    //     UserDetails user = User.withDefaultPasswordEncoder()
+    //         .username("admin")
+    //         .password("admin")
+    //         .roles("ADMINISTRADOR")
+    //         .build();
         
         
-        // JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+    //     // JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
             
-        return new InMemoryUserDetailsManager(user);
+    //     return new InMemoryUserDetailsManager(user);
+    // }
+
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
+        // UserDetails user = User.withDefaultPasswordEncoder()
+        //     .username("user")
+        //     .password("password")
+        //     .roles("FUNCIONARIO")
+        //     .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        // users.createUser(user);
+        return users;
     }
-   
+
+    //salvar usuario no banco
+    // @Bean
+    // public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    //     UserDetails user = User.withDefaultPasswordEncoder()
+    //         .username("admin")
+    //         .password("admin")
+    //         .roles("ADMINISTRADOR")
+    //         .build();
+
+    //     auth.jdbcAuthentication()
+    //         .dataSource(dataSource)
+    //         .withDefaultSchema()
+    //         .withUser(user);
+    // }
+
+    // @Override
+    // protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	// 	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    //     UserDetails user =
+    //              User.builder()
+    //                 .username("nico")
+    //                 .password(encoder.encode("nico"))
+    //                 .roles("ADMINISTRADOR")
+    //                 .build();
+	// 	auth
+	// 		.jdbcAuthentication()
+	// 		.dataSource(dataSource)
+	// 		.passwordEncoder(encoder)
+    //         .withUser(user);
+		
+	// }
 }
--- src/main/java/com/eee/teste/lala/repository/UsuarioRepository.java
+++ src/main/java/com/eee/teste/lala/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.eee.teste.lala.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.eee.teste.lala.model.Usuario;
@@ -9,4 +10,10 @@ import com.eee.teste.lala.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
     Usuario findById(long id);
+
+    // @Query("SELECT authority FROM authorities WHERE username = 'user'")
+    // String findAuthorityByUsername(String username);
+
+   
+    
 }
